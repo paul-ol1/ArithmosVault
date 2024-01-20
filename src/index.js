a8d8fb76ae59393e25cb51b68ee5a7794575112c
paul-ol1 <oladelepaul3@gmail.com>
2024-01-20 21:58:55 +0100

    Initialize ArithmosVault Electron app with React

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app"; // Ensure correct import path
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
